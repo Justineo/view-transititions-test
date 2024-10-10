b937a5973e8bc09ae26f18a2bc7221e7a04e8425
Justineo <justice360@gmail.com>
2024-10-10 16:09:36 +0800

    .

--- js/main.js
+++ js/main.js
@@ -4,7 +4,8 @@ import javascriptLogo from '/javascript.svg'
 document.querySelector('#app').innerHTML = `
   <div>
     <img id="logo" src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" style="view-transition-name: logo; width: 64px; height: 64px;" />
-    <br>
+  </div>
+  <div>
     <a id="link" href="/" style="view-transition-name: link;">Go to JavaScript</a>
   </div>
 `
--- main.js
+++ main.js
@@ -4,7 +4,8 @@ import viteLogo from '/vite.svg'
 document.querySelector('#app').innerHTML = `
   <div>
     <img id="logo" src="${viteLogo}" class="logo" alt="Vite logo" style="view-transition-name: logo; width: 32px; height: 32px;" />
-    <br>
+  </div>
+  <div>
     <a id="link" href="./js/" style="view-transition-name: link;">Go to JavaScript</a>
   </div>
 `
